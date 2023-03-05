15baeea0a32a691ab6eea7e7d62efe1ec24a10ce
Alfredo <alfredo_lajara@outlook.es>
2023-03-05 19:21:08 +0100

    Explota el coche

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,11 @@ public class GameManager : MonoBehaviour
 {
     public bool gameOver = false;
     public int vidasCaco = 4;
+    public ParticleSystem explosion;
     // Start is called before the first frame update
     void Start()
     {
+        explosion = GameObject.Find("Explosion").GetComponent<ParticleSystem>();
         gameOver = false;
         vidasCaco = 4;
     }
@@ -16,6 +18,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (vidasCaco > 0) return;
+        gameOver = true;
     }
 }
--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -9,35 +9,53 @@ public class MoveForward : MonoBehaviour
     public float speed = 5f;
 
     public GameManager gameManager;
+    
+    public ParticleSystem explosion;
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        explosion = GameObject.Find("Explosion").GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * (Time.deltaTime * speed));
+        if (gameManager.vidasCaco <= 0 && explosion.isPlaying == false)       
+        {
+            explosion.Play();
+        }
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
+        if (gameManager.gameOver)
+        {
+            Destroy(gameObject);
+        }
         if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Gallina"))
         {
             gameManager.gameOver = true;
             Destroy(gameObject);
+            
         }
         else if (other.gameObject.CompareTag("Caco") && gameObject.CompareTag("Bala"))
 
         {
             gameManager.vidasCaco--;
+
             Destroy(gameObject);
+            
+            
         } else if (other.gameObject.CompareTag("Finish"))
         {
             Destroy(gameObject);
         }
+
     }
 }
